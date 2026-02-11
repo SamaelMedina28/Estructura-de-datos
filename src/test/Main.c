d7507dddf79b92fb3856809c91e02a8cd9e8599e
Samael Medina Laptop <samaelortiz2218@gmail.com>
2026-02-11 14:11:40 -0800

    Initial commit

---
+++ src/test/Main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "../iostring/iostring.h"
+#include "../structures/lista/lista.h"
+#include "Alumno.h"
+
+int main()
+{
+	printf("Listas enlazadas");
+	Alumno *a,*b,*c;
+	a = crearAlumno();
+	b = crearAlumno();
+	c = crearAlumno();
+
+	Lista lista = {NULL,NULL,0,imprimirAlumno,compararNombre,free};
+	agregarEnOrden(&lista,a);
+	agregarEnOrden(&lista,b);
+	agregarEnOrden(&lista,c);
+	imprimirLista(lista);
+
+	getchar();
+	reordenar( &lista ,compararPromedio );
+	imprimirLista(lista);
+
+
+	return 0;
+}
